be8067b412a668f568ea6768c24e4a8d46db346b
Ksuaza <ksuaza@hotmail.com>
2024-08-26 23:15:13 -0500

    Search page advanced development.

--- src/app/searcher/layout/searcher/searcher.component.ts
+++ src/app/searcher/layout/searcher/searcher.component.ts
@@ -1,16 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { CalendarModule } from 'primeng/calendar';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { DropdownModule } from 'primeng/dropdown';
 
 @Component({
   selector: 'app-searcher',
   templateUrl: './searcher.component.html',
   styleUrl: './searcher.component.css',
-  imports:[
+  imports: [
     RouterOutlet,
-    RouterLink
+    RouterLink,
+    ButtonModule,
+    CalendarModule,
+    InputNumberModule,
+    DropdownModule,
   ],
-  standalone:true
+  standalone: true,
 })
 export class SearcherComponent {
-
+  cities = [
+    { name: 'New York', code: 'NY' },
+    { name: 'Rome', code: 'RM' },
+    { name: 'London', code: 'LDN' },
+    { name: 'Istanbul', code: 'IST' },
+    { name: 'Paris', code: 'PRS' },
+  ];
 }
